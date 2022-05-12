c4c64ed821739acc3fa2d2283c93450d87e94127
Laura Randl <gollumeye@outlook.de>
2022-05-12 15:27:03 +0200

    Sheet 7 - exc2

--- app/src/main/java/com/example/appdev_sheet5_exc2_bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/appdev_sheet5_exc2_bottomnavigation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appdev_sheet5_exc2_bottomnavigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,11 +15,19 @@ import com.example.appdev_sheet5_exc2_bottomnavigation.databinding.ActivityMainB
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int counter = 0;
+
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(counter%2==1) {
+            setTheme(R.style.SecondTheme);
+        }
+        else{
+            setTheme(R.style.FirstTheme);
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        binding.button3.setOnClickListener(e->{
+            counter++;
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
     }
 
 }
